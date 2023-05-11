e1a618f7734df21fb68bc0303872776405fa51cf
Katopy <84346755+Katopy@users.noreply.github.com>
2023-05-11 02:03:21 -0600

    required to user

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -17,23 +17,23 @@
     @csrf
     <div class="mb-3">
       <label for="nombre" class="form-label">Name</label>
-      <input type="text" class="form-control" id="nombre" name="name">
+      <input type="text" class="form-control" id="nombre" name="name" required>
     </div>
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+      <input type="password" class="form-control" id="exampleInputPassword1" name="password" required>
     </div>
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">fecha</label>
-      <input type="date" class="form-control" id="exampleInputPassword1" name="fecha">
+      <input type="date" class="form-control" id="exampleInputPassword1" name="fecha" required>
     </div>
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required>
     </div>
     <div class="mb-3">
       <label for="exampleInputPassword1" class="form-label">Dui</label>
-      <input type="text" class="form-control" id="exampleInputPassword1" name="dui">
+      <input type="text" class="form-control" id="exampleInputPassword1" name="dui" required>
     </div>
   
    
